d3b6462fcb3a91918280953ae82f4c4244e62f74
masanarimorooka <masa_moro2010@yahoo.co.jp>
2016-12-22 18:34:41 +0900

    fix

--- app/models/recommender.rb
+++ app/models/recommender.rb
@@ -1,5 +1,6 @@
 class Recommender < ApplicationRecord
   has_many :products
+  validates :user_name, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
