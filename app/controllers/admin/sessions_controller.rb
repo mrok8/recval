16448b0a3e5be46a8870f7ae887f52595763cbe4
masanarimorooka <masa_moro2010@yahoo.co.jp>
2016-12-22 15:58:23 +0900

    fix device

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -12,9 +12,9 @@ class Admin::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
